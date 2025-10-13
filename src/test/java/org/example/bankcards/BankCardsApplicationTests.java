5a8268e844375e12d98152a3f34e6243b7e4b76c
unknown <kosegor@yandex.ru>
2025-10-13 17:47:11 +0300

    Initial commit

---
+++ src/test/java/org/example/bankcards/BankCardsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bankcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankCardsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
